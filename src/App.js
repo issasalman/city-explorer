74a98a681b883edce874d7ca3a19a01ba9f6c270
issasalman <issasalmanhu1996@gmail.com>
2021-09-05 20:18:01 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Card from "react-bootstrap/Card";
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locationData: {},
+      error: false,
+      locationImg: "",
+    };
+  }
+  submitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const location = e.target.locationName.value;
+      console.log("user Input Location: ", location);
+      const response = await axios.get(
+        `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${location}&format=json`
+      );
+      this.setState({
+        locationData: response.data[0],
+      });
+      const response2 = await axios.get(
+        `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.state.locationData.lat},${this.state.locationData.lon}&format=jpg `
+      );
+      console.log("our img response", response2.data);
+      console.log("our axios response", response.data[0]);
+      this.setState({
+        locationImg: response2.config.url,
+      });
+      console.log("our cs response", this.locationImg);
+    } catch (error) {
+      console.log("catch error" + error);
+      this.setState({
+        error: true,
+      });
+    }
+  };
+  render() {
+    // Solution #2
+    // let imgSrc;
+    // if (this.state.locationData.lon) {
+    //   imgSrc = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.state.locationData.lat},${this.state.locationData.lon}&format=jpg `;
+    // } else {
+    //   imgSrc = "";
+    // }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div>
+        <form onSubmit={this.submitForm}>
+          <label
+            style={{
+              color: "white",
+            }}
+          >
+            Location Name:{" "}
+          </label>
+          <input name="locationName" type="text" placeholder="Enter Location" />
+          <input type="submit" value="Search Location" />
+        </form>
+        <div>
+          <Card
+            style={{
+              width: "30rem",
+              border: "solid",
+              background: "white",
+              color: "white",
+            }}
+          >
+            <Card.Body
+              style={{
+                color: "black",
+              }}
+            >
+              <Card.Title>
+                {" "}
+                Display Name :{this.state.locationData.display_name}
+              </Card.Title>
+              <Card.Text>latitude : {this.state.locationData.lat}</Card.Text>
+              <Card.Text>longitude : {this.state.locationData.lon}</Card.Text>
+              {/* <Card.Img variant="top"  /> */}
+            </Card.Body>
+          </Card>
+        </div>
+        <div>
+        {this.state.error && <p style={{  color: "white",   }}>Location not found try again </p>}
+        {this.state.locationData.lon && <p style={{  color: "white",   }} >The Selected Map </p>} 
+        </div>
+        <div >
+          <img src={this.state.locationImg} alt={""} />
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
